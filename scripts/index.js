28efc392254eb43f4c30f25ef5a29688618875ec
Владимир <vladimir@MacBook-Air-Vladimir.local>
2023-03-04 17:40:13 +0500

    feat: users can add new cards

--- scripts/index.js
+++ scripts/index.js
@@ -26,15 +26,29 @@ const initialPublication = [
   }
 ];
 
-
-const popup = document.querySelector('.popup');
+// variables related to #popup-edit-profile
+const popupEditProfile = document.querySelector('#popup-edit-profile');
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const titleInput = popup.querySelector('#profile-title-input');
-const subtitleInput = popup.querySelector('#profile-subtitle-input');
-const formElement = popup.querySelector('form');
+const titleInput = popupEditProfile.querySelector('#profile-title-input');
+const subtitleInput = popupEditProfile.querySelector('#profile-subtitle-input');
+const editProfileFormElement = popupEditProfile.querySelector('form');
+
+// variables related to #popup-new-place
+const popupNewPlace = document.querySelector('#popup-new-place');
+const addButton = document.querySelector('.profile__add-button');
+const pubInputName = popupNewPlace.querySelector('#new-pub-name');
+const pubInputLink = popupNewPlace.querySelector('#new-pub-link');
+const addNewPlaceFormElement = popupNewPlace.querySelector('form');
+
+// variables related to #popup-big-image
+const popuBigImage = document.querySelector('#popup-big-image');
+
+
+const closeButtonsNodes = document.querySelectorAll('.popup__close-button');
+// console.log(closeButtonsArr);
+
 
 const publicationsNodes = document.querySelector('.publications');
 // console.log(publicationsNodes);
@@ -52,7 +66,7 @@ function prependImageToPage (imgName, imgLink) {
   publicationsNodes.prepend(publication);
 }
 
-function addInitPubToPage (pubArr) {
+function addInitPubsToPage (pubArr) {
   pubArr.forEach(iPublication => {
     prependImageToPage(iPublication.name, iPublication.link);
   });
@@ -62,33 +76,57 @@ function copyTemplateContent(template) {
   return template.content.cloneNode(true);
 }
 
-
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-
-function openPopup() {
-  popup.classList.add('popup_opened');
+function openEditProfilePopup() {
+  popupEditProfile.classList.add('popup_opened');
   titleInput.value = profileTitle.textContent;
   subtitleInput.value = profileSubtitle.textContent;
 }
 
+function openNewPlacePopup() {
+  popupNewPlace.classList.add('popup_opened');
+  pubInputName.value = '';
+  pubInputLink.value = '';
+}
 
-function handleFormSubmit(evt) {
+function handleFormSubmitEditProfile(evt) {
   // This funct. closes form and changes profile__title and profile__subtitle with input values.
+  const thisPopup = evt.target.parentElement.parentElement;
   evt.preventDefault();
   
   profileTitle.textContent = titleInput.value;
   profileSubtitle.textContent = subtitleInput.value;
-  
-  closePopup();
+  closePopup(thisPopup);
+}
+
+function handleFormSubmitAddNewPlace(evt) {
+    // This funct. closes form and addes new publication (place) with input values.
+    const thisPopup = evt.target.parentElement.parentElement;
+    evt.preventDefault();
+
+    prependImageToPage(pubInputName.value, pubInputLink.value);
+    closePopup(thisPopup);
 }
 
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
 
-addInitPubToPage(initialPublication);
+editButton.addEventListener('click', openEditProfilePopup);
+addButton.addEventListener('click', openNewPlacePopup); 
+
+// closeButton is a Nodes collection, so this EventListener applies to every close button (X) on the page.
+closeButtonsNodes.forEach(iCloseButton => {
+  iCloseButton.addEventListener('click', evt => {
+    const thisPopup = evt.target.parentElement.parentElement.parentElement;
+    closePopup(thisPopup);
+  });
+});
+
+editProfileFormElement.addEventListener('submit', handleFormSubmitEditProfile);
+addNewPlaceFormElement.addEventListener('submit', handleFormSubmitAddNewPlace);
+
+
+addInitPubsToPage(initialPublication);
 
