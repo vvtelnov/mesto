0c509bdbb65388a287594ca9bd8a5418c26ab57b
Владимир <vladimir@MacBook-Air-Vladimir.local>
2023-03-04 12:47:06 +0500

    feat: images load to page with JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
+// This is a array of cards that appears on the page when first opened or reloaded.
+const initialPublication = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
@@ -7,16 +36,44 @@ const titleInput = popup.querySelector('#profile-title-input');
 const subtitleInput = popup.querySelector('#profile-subtitle-input');
 const formElement = popup.querySelector('form');
 
+const publicationsNodes = document.querySelector('.publications');
+// console.log(publicationsNodes);
+const publicationTemplate = document.querySelector('#publication-template');
+
+
+function prependImageToPage (imgName, imgLink) {
+  const publication = copyTemplateContent(publicationTemplate);
+  imgName = String(imgName);
+  imgLink = String(imgLink);
+
+  publication.querySelector('.publication__photo').src = imgLink;
+  publication.querySelector('.publication__title').textContent = imgName;
+
+  publicationsNodes.prepend(publication);
+}
+
+function addInitPubToPage (pubArr) {
+  pubArr.forEach(iPublication => {
+    prependImageToPage(iPublication.name, iPublication.link);
+  });
+}
+
+function copyTemplateContent(template) {
+  return template.content.cloneNode(true);
+}
+
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
+
 
 function openPopup() {
   popup.classList.add('popup_opened');
   titleInput.value = profileTitle.textContent;
   subtitleInput.value = profileSubtitle.textContent;
 }
+
 
 function handleFormSubmit(evt) {
   // This funct. closes form and changes profile__title and profile__subtitle with input values.
@@ -32,3 +89,6 @@ function handleFormSubmit(evt) {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
+
+addInitPubToPage(initialPublication);
+
